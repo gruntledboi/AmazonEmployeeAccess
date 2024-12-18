28fbefaceddc7b15eaf9731d2d5d2368347025a1
gruntledboi <cotothetrell48@gmail.com>
2024-12-18 16:52:14 -0700

    the SVM file!

---
+++ AmazonSVM.R
@@ -0,0 +1,79 @@
+library(tidymodels)
+library(tidyverse)
+library(vroom)
+library(embed)
+library(lme4)
+library(parsnip)
+library(discrim)
+library(kernlab)
+
+
+trainData <- vroom("train.csv")
+testData <- vroom("test.csv")
+
+trainData$ACTION <- as.factor(trainData$ACTION)
+
+my_recipe <- recipe(ACTION ~ ., data = trainData) |> 
+  step_mutate_at(all_numeric_predictors(), fn = factor) |>  # turn all numeric features into factors
+  step_other(all_nominal_predictors(), threshold = .001) |>  # combines categorical values that occur
+  #step_dummy(all_nominal_predictors()) |>  # dummy variable encoding
+  #step_mutate_at(ACTION, fn = factor) |> 
+  step_lencode_mixed(all_nominal_predictors(), 
+                     outcome = vars(ACTION)) |> 
+  step_normalize(all_numeric_predictors())
+
+#target encoding (must
+# also step_lencode_glm() and step_lencode_bayes()
+
+
+# NOTE: some of these step functions are not appropriate to use together
+
+# apply the recipe to your data
+prep <- prep(my_recipe)
+baked <- bake(prep, new_data = trainData)
+
+
+## SVM models
+svmLinear <- svm_linear(cost = tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+
+svm_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(svmLinear)
+
+
+tuning_grid <- grid_regular(cost(),
+                            levels = 3)
+
+## Set up K-fold CV
+folds <- vfold_cv(trainData, v = 3, repeats = 1)
+
+CV_results <- svm_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = NULL)
+
+## Find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "roc_auc")
+
+
+## Predict
+final_wf <-
+  svm_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = trainData)
+
+
+amazon_svm_predictions <- 
+  predict(final_wf,
+          new_data = testData,
+          type = "prob") |> 
+  bind_cols(testData) |> 
+  rename(ACTION = .pred_1) |> 
+  select(id, ACTION)
+
+vroom_write(x = amazon_svm_predictions, 
+            file="./AmazonSVMPreds.csv", delim=",")
